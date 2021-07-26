23561c773a913ad4fd15bf39b2899a5fad952ff7
ismail harmanda <iharmanda@hotmail.com>
2021-07-26 20:13:43 +0300

    logic done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Todo from "./components/Todo";
 
 const App = () => {
   return (
     <div>
       <h1>My Todos</h1>
+      <Todo text="Learn Reac" />
     </div>
   );
 };
